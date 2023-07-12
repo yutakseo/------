4f8a6dfaeb77c6fc45e6ec93b5f16707107be534
Yutak_Seo <soano2428@gmail.com>
2023-07-12 21:35:43 +0900

    23.07.12

---
+++ 10869.py
@@ -0,0 +1,12 @@
+#두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+#입력 : 두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+#출력 : 첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+
+res = input().split(" ")
+A = int(res[0])
+B = int(res[1])
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
